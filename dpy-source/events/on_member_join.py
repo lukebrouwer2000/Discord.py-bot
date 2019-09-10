cf7702c478a67a557c22057fa0ab3d838e9b870e
lukebrouwer2000 <53581714+lukebrouwer2000@users.noreply.github.com>
2019-09-10 00:20:00 -0700

    Update on_member_join.py
    changed event to only occur in "general" channel

--- dpy-source/events/on_member_join.py
+++ dpy-source/events/on_member_join.py
@@ -1,5 +1,8 @@
 @client.event
 async def on_member_join(member):
     """adds a welcome message when a new user joins the server"""
-    print(f'{member} has joined a server.')                           # prints in the console
-    await member.send(f'{member} has joined the server!')             # prints in the guild chat
+    for channel in member.guild.channels:
+        if str(channel) == "general":
+            print(f'{member} has joined the server.')
+            await member.send(f'{member} has joined the server!')            # prints in the console
+            await member.send_message(f'Welcome to the server!')             # prints in the guild chat
