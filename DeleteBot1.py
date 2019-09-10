89b867d6a74cab187285fe8b8218076833927da9
lukebrouwer2000 <53581714+lukebrouwer2000@users.noreply.github.com>
2019-09-10 00:20:40 -0700

    Update DeleteBot1.py

--- DeleteBot1.py
+++ DeleteBot1.py
@@ -6,9 +6,9 @@ import time
 client = commands.Bot(command_prefix="!")
 
 
-@client.command()
-async def load(ctx, extension):
-    client.load_extension(f'cogs.{extension}')
+# @client.command()
+# async def load(ctx, extension):
+#     client.load_extension(f'cogs.{extension}')
 
 
 @client.event
@@ -23,65 +23,125 @@ async def on_ready():
 @client.event
 async def on_member_join(member):
     """adds a welcome message when a new user joins the server"""
-    print(f'{member} has joined a server.')
-    await member.send(f'{member} has joined the server!')
+    for channel in member.guild.channels:
+        if str(channel) == "general":
+            print(f'{member} has joined the server.')
+            await member.send(f'{member} has joined the server!')
+            await member.send_message(f'Welcome to the server!')
 
 
 @client.event
 async def on_member_remove(member):
     """adds a goodbye message when an existing user leaves the server"""
-    print(f'{member} has left a server.')
+    print(f'{member} has left the server.')
     await member.send(f'{member} has left the server!')
 
 
 @client.event
 async def on_message_delete(message):
     """creates an embed when a message is deleted in the channel"""
-    #
-    client.get_channel(620010049422753812)  # post embed in specific channel
-    embed = discord.Embed()
-    embed.set_author(name=message.author, icon_url=message.author.avatar_url)  # header
-    embed.description = message.content  # description
-    embed.set_footer(text='Message seen in '
-                          + message.guild.name + " in "
-                          + message.channel.name + "\n| \nYou've been caught by Vive bot")  # footer
+    allowable_channels = ["general"]
+    if str(message.channel) in allowable_channels:
 
-    if len(message.content) > 0:
-        embed.description = message.content
-    else:
-        embed.description = 'No message content'
+        client.get_channel(620010049422753812)  # post embed in specific channel
+        embed = discord.Embed()
+        embed.set_author(name=message.author, icon_url=message.author.avatar_url)  # header
+        embed.description = message.content  # description
 
-    await message.channel.send(embed=embed)
+        embed.set_footer(text=f'Message seen in {message.guild.name} in {message.channel.name} | '
+                              f"\nYou've been caught by Vive bot")
+
+        if len(message.content) > 0:
+            embed.description = message.content
+        else:
+            embed.description = 'No message content'
+
+        await message.channel.send(embed=embed)
 
 
 @client.command()
 async def ping(ctx):
     """allows user to verify ping associated with discord server"""
-    await ctx.send(f'Pong! Your ping to discord servers :  {round(client.latency * 1000)}ms')
+    allowable_channels = ["general"]
+    if str(ctx.channel) in allowable_channels:
+        await ctx.send(f'Pong! Your ping to discord servers :  {round(client.latency * 1000)}ms')
 
 
 @client.command()
 async def clear(ctx, amount=5000):
     """clears all messages in chat based on amount=value"""
-    start_time = time.time()
-    purge_count = await ctx.channel.purge(limit=amount)
-    end_time = time.time()
+    allowable_channels = ["general"]
+    if str(ctx.channel) in allowable_channels:
+        start_time = time.time()
+        purge_count = await ctx.channel.purge(limit=amount)
+        end_time = time.time()
 
-    await ctx.channel.send('Deleted {} messages in channel {}'
-                           .format(len(purge_count), ctx.channel.mention))
-    await ctx.channel.send('Purge took {0:0.1f} seconds'
-                           .format(end_time - start_time))
+        await ctx.channel.send('Deleted {} messages in channel {}'
+                               .format(len(purge_count), ctx.channel.mention))
+        await ctx.channel.send('Purge took {0:0.1f} seconds'
+                               .format(end_time - start_time))
 
 
 @client.command()
-async def clear2(ctx, amount=5000):
-    """clears all links and images"""
-    my_channel = ctx.message.channel
-    messages = []
-    for message in client.history(my_channel, limit=int(amount)):
-        messages.append(message)
-    await ctx.message.delete(messages)
-    await ctx.my_channel.send('Bad links and images deleted!')
+async def kick(ctx, member: discord.Member, *, reason=None):  # reason = none for easy kicking; no context
+    """allows admin users to kick other members while providing reason"""
+    if reason is None:  # reason = none handling
+        none_embed = discord.Embed()
+        ctx.content = f'Dear {member}, \n You were kicked from the server {ctx.guild.name} because you were a dickhead.'
+        none_embed.description = ctx.content
+        await member.send(embed=none_embed)
+
+    else:  # if reason is stated
+        await ctx.channel.send(f'{member} was kicked from the server.')
+
+        embed = discord.Embed()
+        ctx.content = f'Dear {member}: \nYou were kicked from the server, ' \
+                      f'{ctx.guild.name}, because: {reason}.'
+        embed.description = ctx.content
+
+        await member.send(embed=embed)
+        # await member.kick(reason=reason)
+
+
+@client.command()
+async def ban(ctx, member: discord.Member, *, reason=None):
+    """allows admin users to ban other members while providing reason"""
+    if reason is None:  # reason = none handling
+        await ctx.channel.send(f'{member} was banned from the server.')  # declares user was banned in text channel
+        none_embed = discord.Embed()
+        ctx.content = f'Dear {member}, \n you recently used an unacceptable word in the server {ctx.guild.name}.' \
+                      f' Due to this infraction, you have been banned. Rest in pieces, {member}. Such a woefully ' \
+                      f'short life.'
+        none_embed.description = ctx.content
+        await member.send(embed=none_embed)
+
+    else:  # if reason is stated
+        await ctx.channel.send(f'{member} was banned from the server.')
+        embed = discord.Embed()
+        ctx.content = f'Dear {member}, \n you were kicked from the server because of these reasons :' \
+                      f'\n {reason}'
+        embed.description = ctx.content
+        await member.send(embed=embed)
+        #  await member.ban(reason=reason)
+
+
+# @client.command()
+# async def ban(ctx, member: discord.Member, *, reason=None):  # reason = none for easy banning; no context
+#     await member.ban(reason=reason)
+
+
+ident = client.get_guild(616811413268070415)
+client.run('NjE3MTIwNDkxMDUxOTQxOTI4.XXcs4A.ASCdHptmjx3Zu3_JHf1csBrXwQA')
+
+# @client.command()
+# async def clear2(ctx, amount=5000):
+#     """clears all links and images"""
+#     my_channel = ctx.message.channel
+#     messages = []
+#     for message in client.history(my_channel, limit=int(amount)):
+#         messages.append(message)
+#     await ctx.message.delete(messages)
+#     await ctx.my_channel.send('Bad links and images deleted!')
 
 
 # @client.event
@@ -93,6 +153,3 @@ async def clear2(ctx, amount=5000):
 #
 #     embed.set_author(name=message.author, icon_url=message.author.avatar_url)
 #     embed.description = message.content
-
-
-client.run('NjE3MTIwNDkxMDUxOTQxOTI4.XXNNag.m3jABNznKgas5hTBW4oI3SKTIMY')
