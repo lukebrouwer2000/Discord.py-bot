64a12f3393e681981d1890f7f4bbf7e8eb89e6db
lukebrouwer2000 <53581714+lukebrouwer2000@users.noreply.github.com>
2019-09-10 00:16:30 -0700

    Create ban.py

---
+++ dpy-source/commands/ban.py
@@ -0,0 +1,25 @@
+@client.command()
+async def ban(ctx, member: discord.Member, *, reason=None):
+    """allows admin users to ban other members while providing reason"""
+    if reason is None:  # reason = none handling
+        await ctx.channel.send(f'{member} was banned from the server.')  # declares user was banned in text channel
+        
+        none_embed = discord.Embed()
+        ctx.content = f'Dear {member}, \n you recently used an unacceptable word in the server {ctx.guild.name}.' \
+                      f' Due to this infraction, you have been banned. Rest in pieces, {member}. Such a woefully ' \
+                      f'short life.'
+        none_embed.description = ctx.content
+        
+        await member.send(embed=none_embed)
+
+    else:  # if reason is stated
+    
+        await ctx.channel.send(f'{member} was banned from the server.')  # prints to guild channel before ban
+        
+        embed = discord.Embed()
+        ctx.content = f'Dear {member}, \n you were kicked from the server because of these reasons :' \
+                      f'\n {reason}'
+        embed.description = ctx.content
+        
+        await member.send(embed=embed)
+        await member.ban(reason=reason)
