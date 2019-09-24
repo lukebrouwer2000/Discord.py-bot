74e22ac8ec25f5e678d1b55f2f9ae93e01d4ab92
lukebrouwer2000 <53581714+lukebrouwer2000@users.noreply.github.com>
2019-09-24 16:13:23 -0700

    updated clear command
    switched from .format functionality to f'strings
    
    added allowable_channels variable to choose which channels the bot can work in

--- dpy-source/commands/purge.py
+++ dpy-source/commands/purge.py
@@ -2,11 +2,12 @@
 @commands.has_role(role)
 async def clear(ctx, amount=5000):
     """clears all messages in chat based on amount=value"""
-    start_time = time.time()                                                    # execution timer start
-    purge_count = await ctx.channel.purge(limit=amount)                         # purge code to run
-    end_time = time.time()                                                      # execution timer stop
+    allowable_channels = [channels]
+    
+    if str(ctx.channel) in allowable_channels:
+        start_time = time.time()
+        purge_count = await ctx.channel.purge(limit=amount)
+        end_time = time.time()
 
-    await ctx.channel.send('Deleted {} messages in channel {}'                  
-                           .format(len(purge_count), ctx.channel.mention))
-    await ctx.channel.send('Purge took {0:0.1f} seconds'
-                           .format(end_time - start_time))
+        await ctx.channel.send(f'Deleted {len(purge_count)} messages in channel {ctx.channel.mention}')
+        await ctx.channel.send(f'Purge took {(end_time - start_time):0.2f} seconds')  # execution time rounded to 2 places, works w/ float
