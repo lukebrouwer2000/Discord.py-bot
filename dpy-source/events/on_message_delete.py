10fcf2c114f575ebeb0bbd860422425cff517f42
lukebrouwer2000 <53581714+lukebrouwer2000@users.noreply.github.com>
2019-09-24 19:18:59 -0700

    removed identifiers

--- dpy-source/events/on_message_delete.py
+++ dpy-source/events/on_message_delete.py
@@ -1,17 +1,15 @@
 @client.event
 async def on_message_delete(message):
     """creates an embed when a message is deleted in the channel"""
-    
-    client.get_channel(620010049422753812)                                                          # post embed in specific channel
+                                                           
     embed = discord.Embed()
-    embed.set_author(name=message.author, icon_url=message.author.avatar_url)                       # header
-    embed.description = message.content  # description
+    embed.set_author(name=message.author, icon_url=message.author.avatar_url)                       
     embed.set_footer(text=f"Message seen in {message.guild.name} in {message.channel.name} | "
-                              f"\nYou've been caught by Vive bot")                                  # footer
+                              f"\nYou've been caught by Vive bot")                                  
 
     if len(message.content) > 0:                                                                    
-        embed.description = message.content                                                         # if length of message > 0
+        embed.description = message.content                                                         
     else:
-        embed.description = 'No message content'                                                    # all other conditions
+        embed.description = 'No message content'                                                    
   
-    await message.channel.send(embed=embed)                                                         # prints embed in channel
+    await message.channel.send(embed=embed) # prints embed in channel
