0fd2f640c4e1964601c5fd3b66d5e7598f7cbda0
lukebrouwer2000 <53581714+lukebrouwer2000@users.noreply.github.com>
2019-09-27 16:12:25 -0700

    small decorator update
    added decorator to check if bot owner

--- dpy-source/commands/kick.py
+++ dpy-source/commands/kick.py
@@ -1,23 +1,22 @@
 @client.command()
-@commands.has_role(role)
-async def kick(ctx, member: discord.Member, *, reason=None):  # reason = none for easy execution; no context
+@commands.is_owner()
+@commands.has_role("Admin")
+async def kick(ctx, member: discord.Member, *, reason=None):  # reason = none for easy kicking; no context
     """allows admin users to kick other members while providing reason"""
-    
-    if reason is None:  # reason = none handling
-    
+    # reason = none exception handling
+    if reason is None:  
         none_embed = discord.Embed()
-        ctx.content = f'Dear {member}, \n You were kicked from the server {ctx.guild.name} because y____________.'
+        ctx.content = f'Dear {member}, \n You were kicked from the server {ctx.guild.name} because ...'
         none_embed.description = ctx.content
-        
         await member.send(embed=none_embed)
-        
-    else:  # if reason is stated
+
+    else:  
         await ctx.channel.send(f'{member} was kicked from the server.')
-        
+
         embed = discord.Embed()
         ctx.content = f'Dear {member}: \nYou were kicked from the server, ' \
                       f'{ctx.guild.name}, because: {reason}.'
         embed.description = ctx.content
-        
+
         await member.send(embed=embed)
         await member.kick(reason=reason)
