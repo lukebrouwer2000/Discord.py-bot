8ca76d445d8f838d59daac1f172a2029f114aa8a
lukebrouwer2000 <53581714+lukebrouwer2000@users.noreply.github.com>
2019-10-04 21:13:12 -0700

    big clear updates
    sends deleted content data to set channel, bot pins Discord ToS message after entering in deleted content channel

--- dpy-source/commands/purge.py
+++ dpy-source/commands/purge.py
@@ -1,13 +1,21 @@
 @client.command()
-@commands.has_role(role)
-async def clear(ctx, amount=5000):
+@commands.has_role("Admin")
+@commands.is_owner()
+async def clear(ctx, amount=5000): # default value set to 5k automatically
     """clears all messages in chat based on amount=value"""
-    allowable_channels = [channels]
-    
+    channel_send = client.get_channel(628749903220310016)
+    allowable_channels = ["general", "todo", "development"]
+
+    today = date.today()
+
+    day = today.strftime("%B %d, %Y")
+
     if str(ctx.channel) in allowable_channels:
         start_time = time.time()
         purge_count = await ctx.channel.purge(limit=amount)
         end_time = time.time()
 
-        await ctx.channel.send(f'Deleted {len(purge_count)} messages in channel {ctx.channel.mention}')
-        await ctx.channel.send(f'Purge took {(end_time - start_time):0.2f} seconds')  # execution time rounded to 2 places, works w/ float
+        await channel_send.send(f'Scheduled purge deleted {len(purge_count)} messages in channel {ctx.channel.mention}. \nPurge took {(end_time - start_time):0.2f} seconds. \nToday\'s date: {day}')
+        
+        message = await ctx.channel.send("While this channel is labeled as NSFW breaking discord's Terms of Services (<https://discordapp.com/terms>) will not be allowed.")
+        await message.pin()
