08dd491deff29333c8c214d0e0097bcb1d414d19
lukebrouwer2000 <53581714+lukebrouwer2000@users.noreply.github.com>
2019-10-04 21:14:25 -0700

    big embed changes
    added two new send channels: if message content includes bad word, sends to other channel than if content is simply deleted.

--- dpy-source/events/on_message_delete.py
+++ dpy-source/events/on_message_delete.py
@@ -1,15 +1,23 @@
 @client.event
 async def on_message_delete(message):
     """creates an embed when a message is deleted in the channel"""
-                                                           
-    embed = discord.Embed()
-    embed.set_author(name=message.author, icon_url=message.author.avatar_url)                       
-    embed.set_footer(text=f"Message seen in {message.guild.name} in {message.channel.name} | "
-                              f"\nYou've been caught by Vive bot")                                  
 
-    if len(message.content) > 0:                                                                    
-        embed.description = message.content                                                         
+    bad_send = client.get_channel(628755221266563072)
+    regular_send = client.get_channel(628758306042675200)
+
+    embed = discord.Embed()
+    embed.set_author(name=message.author, icon_url=message.author.avatar_url)  
+    embed.description = message.content  
+    embed.set_footer(text=f'Message seen in {message.guild.name} in {message.channel.name} | '
+                        f"\nYou've been caught by Vive bot")
+
+    if len(message.content) > 0:
+        embed.description = message.content
     else:
-        embed.description = 'No message content'                                                    
-  
-    await message.channel.send(embed=embed) # prints embed in channel
+        embed.description = 'No message content'
+
+    bad_word = ''
+    if bad_word in message.content:
+        await bad_send.send(embed=embed)
+    else: 
+        await regular_send.send(embed=embed)
